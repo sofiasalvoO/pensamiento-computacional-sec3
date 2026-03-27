478bdf7fbae3ef9ac22b195c58724d91d65d26b2
sofiasalvoO <sofia.salvo@mail.udp.cl>
2026-03-27 12:18:40 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+  createCanvas(420, 500);
+}
+
+function draw() {
+  background(220);
+  rect(0,0,420,20)
+  rect(0,20,420,150)
+  rect(0,170,420,150)
+  rect(0,320,420,150)
+  rect(0,480,420,20)
+}
